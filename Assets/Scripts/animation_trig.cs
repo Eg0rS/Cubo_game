c617d2036479fe882701cddc90fb2ed73e71e36c
Eg0rS <Ehrgit@yandex.ru>
2020-11-02 20:05:47 +0300

    my first game

---
+++ Assets/Scripts/animation_trig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_trig : MonoBehaviour
+{
+        public GameObject Obj;
+        public string NameAnimation;
+        private AudioSource aud;
+    void Start() {
+        aud = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Sphere") {
+            Obj.GetComponent<Animation>().Play(NameAnimation);
+            aud.Play ();
+        }
+    }
+}
---
+++ Assets/Scripts/football.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class football : MonoBehaviour
+{
+    public int score = 0;
+    public Text ScoreText;
+    private AudioSource aud;
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+    void Update() {
+        ScoreText.text ="Score " + score;
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("goal")) {
+            score++;
+            aud.Play();
+        }
+    }
+}
